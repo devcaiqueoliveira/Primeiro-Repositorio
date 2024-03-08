3b69d6b201018726d56011e39ba509c56b021d3c
Caique Oliveira <devcaiqueoliveira@gmail.com>
2024-03-08 19:02:47 -0300

    Inicio as aulas de js da faculdade

---
+++ faculdade-js/script.js
@@ -0,0 +1,10 @@
+let idade;
+idade= '35';
+console.log(idade);
+
+//* constante nao pode ser mudada//
+const nome = 'Luigi';
+console.log(nome);
+//* constante nao pode ser mudada//
+
+console.log("Olá " + nome);
