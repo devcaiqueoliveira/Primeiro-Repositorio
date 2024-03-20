21a860c288b007e1d33b1682b4313472caf1b6fd
Caique Oliveira <devcaiqueoliveira@gmail.com>
2024-03-20 19:07:33 -0300

    Sintaxe JavaScript

---
+++ FACULDADE/JAVASCRIPT/faculdade-sintaxe/script.js
@@ -0,0 +1,28 @@
+const nome= "Mario";
+console.log(nome);
+
+let idade;
+idade=30;
+idade = idade+1
+console.log(idade)
+
+let mensagem = "Ola " + nome + ".";
+console.log(mensagem)
+
+var nome2;
+
+nome2= "Luigi";
+
+
+let mensagem2 = `Ola ${nome2}.`;
+console.log(mensagem2)
+
+
+/*
+if(idade >= 18){
+    console.log("Sejam Bem-Vindo")
+}
+else{
+    console.log("Acesso bloqueado")
+}
+*/
