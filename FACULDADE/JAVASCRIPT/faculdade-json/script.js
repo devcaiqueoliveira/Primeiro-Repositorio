5ef70a9df55f04c1b6989d728e8815eb35a8a8b0
Caique Oliveira <devcaiqueoliveira@gmail.com>
2024-03-25 09:55:21 -0300

    Aprendendo sobre JSON

---
+++ FACULDADE/JAVASCRIPT/faculdade-json/script.js
@@ -0,0 +1,15 @@
+let listaDesejos2 = ["Carro",5, "Bicicleta", "Moto"];
+
+let pessoa= { nome:"Mario",
+              idade: 30,
+              peso: "32kg",
+              lista: listaDesejos2};
+
+pessoa.nome="Luigi";
+console.log (pessoa);
+console.log (pessoa.nome);
+console.log (pessoa["peso"]);
+
+let listaDesejos = ["Carro",1, "Apartamento", "Moto"];
+
+console.log (listaDesejos);
